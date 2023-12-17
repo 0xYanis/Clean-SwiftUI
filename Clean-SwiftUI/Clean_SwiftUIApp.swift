863f442deb52117e362fc24391e41ec3c2aba2b0
Yanis Rybkin <ynchprod@gmail.com>
2023-12-17 13:46:32 +0300

    Initial Commit

---
+++ Clean-SwiftUI/Clean_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_SwiftUIApp.swift
+//  Clean-SwiftUI
+//
+//  Created by Yanis on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Clean_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
