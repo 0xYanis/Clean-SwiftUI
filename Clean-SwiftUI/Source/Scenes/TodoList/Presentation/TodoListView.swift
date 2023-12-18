4b9b38c9f83144cd87095d2252f5392e7e1e41a5
Yanis Rybkin <ynchprod@gmail.com>
2023-12-18 18:34:13 +0300

    Updated UI

--- Clean-SwiftUI/Source/Scenes/TodoList/Presentation/TodoListView.swift
+++ Clean-SwiftUI/Source/Scenes/TodoList/Presentation/TodoListView.swift
@@ -16,40 +16,48 @@ struct TodoListView: View {
     var coordinator: Coordinator
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 15) {
+        List {
+            Section {
                 TodoListProfile(person: viewModel.person)
             }
-            .modifier(CellModifier())
             
-            Text("Todo notes")
-                .font(.title2)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding([.leading, .bottom])
-            
-            ForEach(viewModel.todos) { todo in
-                TodoListCell(todo: todo)
+            Section {
+                ForEach(viewModel.todos) { todo in
+                    Label {
+                        Text(todo.title)
+                    } icon: {
+                        systemImage(completed: todo.completed)
+                    }
+                }
+            } header: {
+                Text("Todo")
             }
-            Spacer()
+
         }
-        .searchable(text: $viewModel.searchText)
         .onAppear(perform: viewModel.request)
         .navigationTitle(viewModel.person.name)
-        .navigationBarTitleDisplayMode(.inline)
+        .searchable(text: $viewModel.searchText)
+    }
+    
+    private func systemImage(completed: Bool) -> some View {
+        Image(systemName: completed ? "checkmark.seal.fill" : "xmark.seal")
+            .foregroundStyle(.white, completed ? .green : .red)
     }
     
 }
 
 struct TodoListProfile: View {
-    
     var person: UserListModel.ViewModel
     
     var body: some View {
         HStack {
-            Rectangle()
-                .fill(Color.secondary)
-                .frame(width: 45, height: 45)
-                .cornerRadius(6)
+            ZStack {
+                Rectangle()
+                    .fill(Color.secondary.opacity(0.15))
+                    .frame(width: 45, height: 45)
+                    .cornerRadius(6)
+                Text("🤗")
+            }
             
             VStack(alignment: .leading, spacing: 6) {
                 Text(person.name)
@@ -59,48 +67,8 @@ struct TodoListProfile: View {
             
             Spacer()
         }
-        .padding(10)
     }
     
-}
-
-struct TodoListCell: View {
-    
-    var todo: TodoListModel.ViewModel
-    
-    var body: some View {
-        VStack(alignment: .leading) {
-            HStack(spacing: 15) {
-                Text("\(todo.id)")
-                    .padding(.leading, 10)
-                    .font(.caption)
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text(todo.title)
-                    
-                    Text(todo.completed.description)
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                        .lineLimit(1)
-                }
-                Spacer()
-            }
-            .padding(5)
-        }
-        .modifier(CellModifier())
-    }
-    
-}
-
-struct CellModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .frame(maxWidth: .infinity)
-            .background(Color.secondary.opacity(0.1))
-            .cornerRadius(6)
-            .padding(.horizontal)
-            .padding(.bottom, 5)
-    }
 }
 
 struct TodoListView_Previews: PreviewProvider {
